579969a0fdecc7afdc4ebf049b5c6eafbb461f89
avrians <avriansyahb10@gmail.com>
2023-05-13 06:15:12 +0700

    update hello kotlin

---
+++ src/main/kotlin/com/dicoding/exam/KotlinFundamental1/HelloKotlin.kt
@@ -0,0 +1,34 @@
+package com.dicoding.exam.KotlinFundamental1
+
+// main function
+fun main() {
+    println("Hello Kotlin!")
+
+// koment tunggal
+
+/*
+    Multiple comment bisa
+    lebih dari
+    satu baris
+ */
+
+
+//=======================================
+    println("===========================\n")
+
+    val name = "Alfian"
+
+    print("Hello my name is ")
+    println(name)
+    print(if (true) "Always true" else "Always false")
+}
+
+/*
+   output:
+   Hello Kotlin!
+
+   ====================================
+
+   Hello my name is Alfian
+   Always true
+*/
