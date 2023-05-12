a9571e1337a1d6b17f3fc2afd7a52dea0e956c6b
avrians <avriansyahb10@gmail.com>
2023-05-13 06:34:32 +0700

    finish data types and variabel

--- src/main/kotlin/com/dicoding/exam/KotlinFundamental1/DataTypes&Variable.kt
+++ src/main/kotlin/com/dicoding/exam/KotlinFundamental1/DataTypes&Variable.kt
@@ -1,5 +1,42 @@
 package com.dicoding.exam.KotlinFundamental1
 
-fun main() {
+/*
+    Rangkuman Materi
+    sebuah variabel membutuhkan ata kunci var atau val, identifier, type dan initialization.
+    var identifier: Type = initialization
 
+    contoh variabel dengan tipe String:
+    var company: String = "Dicoding"
+
+    Val atau val
+    var kita bisa mengubah nilai yang sudah kita inisialisasikan.
+    sedangkan val, kita tidak bisa mengubah nilai yang sebelumnya sudah kita inisialisasi.
+
+    Identifier
+    Identifier merupakan nama dari sebuah variabel.
+
+    Type
+    Tipe data dibutuhkan agar kompiler dapat mengetahui bagaimana sebuah data akan digunakan
+ */
+
+
+fun main() {
+    val firstWord = "Dicoding "
+    val lastWord = "Academy"
+    println(firstWord + lastWord)
+
+    println("\n===CONTOH KE 2===\n")
+
+    val valueA: Int = 10
+    val valueB = 20
+    print(valueA + valueB)
 }
+
+/*
+   output:
+   Dicoding Academy
+
+   ===CONTOH KE 2===
+
+   30
+*/
