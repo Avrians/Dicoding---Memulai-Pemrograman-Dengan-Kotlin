a6c19b73c197fb742b393bfb92e9728bf4a28ea6
avrians <avriansyahb10@gmail.com>
2023-05-14 14:39:07 +0700

    finish sub bab While dan Do While

---
+++ src/main/kotlin/com/dicoding/exam/ControlFlow2/WhiledanDoWhile.kt
@@ -0,0 +1,35 @@
+package com.dicoding.exam.ControlFlow2
+
+fun main() {
+    var counter = 1
+    while (counter <= 7){
+        println("Hello, World!")
+        counter++
+    }
+    /*
+   output :
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+    */
+
+    do {
+        println("Hello, World!")
+        counter++
+    } while (counter <= 7)
+    /*
+   output:
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+       Hello, World!
+    */
+
+}
