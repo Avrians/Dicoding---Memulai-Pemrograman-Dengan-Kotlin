83a6c3eaf2ca6c06a2cbdad9dd41d460a2bfd7b0
avrians <avriansyahb10@gmail.com>
2023-05-14 17:22:49 +0700

    finish sub bab Destructuring Declarations

--- src/main/kotlin/com/dicoding/exam/DataClasses3/DataClasses.kt
+++ src/main/kotlin/com/dicoding/exam/DataClasses3/DataClasses.kt
@@ -21,7 +21,11 @@ class User(val name : String, val age : Int){
     }
 }
 
-data class DataUser(val name : String, val age : Int)
+data class DataUser(val name : String, val age : Int) {
+    fun intro(){
+        println("My name is $name, I am $age years old")
+    }
+}
 
 fun main(){
     val user = User("nrohmen", 17)
---
+++ src/main/kotlin/com/dicoding/exam/DataClasses3/DestructuringDeclarations.kt
@@ -0,0 +1,17 @@
+package com.dicoding.exam.DataClasses3
+
+fun main() {
+    val dataUser = DataUser("nrohmen", 17)
+
+    val name = dataUser.component1()
+    val age = dataUser.component2()
+
+    // Kita juga dapat membuat beberapa variabel dari objek secara langsung dengan kode seperti berikut:
+    val (name2, age2) = dataUser
+
+    println("My name is $name, I am $age years old")
+    println("My name is $name2, I am $age2 years old")
+
+    val dataUser2 = DataUser("nrohmen", 23)
+    dataUser2.intro()
+}
