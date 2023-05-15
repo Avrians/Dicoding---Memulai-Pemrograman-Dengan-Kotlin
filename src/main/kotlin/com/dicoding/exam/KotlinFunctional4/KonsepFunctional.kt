9fa6b63edee0e2944334f4c0575365dce6d821b4
avrians <avriansyahb10@gmail.com>
2023-05-15 11:52:49 +0700

    finish sub bab Konsep Functional Programming

---
+++ src/main/kotlin/com/dicoding/exam/KotlinFunctional4/KonsepFunctional.kt
@@ -0,0 +1,19 @@
+package com.dicoding.exam.KotlinFunctional4
+
+// bahasa pemrograman berorientasi objek
+// val list = getListUser()
+
+//fun getUsername(): List<String>{
+//    val name = mutableListOf<String>()
+//    for (user in list){
+//        name.add(user.name)
+//    }
+//    return name
+//}
+
+// gaya fungsional
+//fun getUsername(): List<String>{
+//    return list.map {
+//        it.name
+//    }
+//}
