70148b818a670440d4126d48468387d1c4457437
avrians <avriansyahb10@gmail.com>
2023-05-17 10:19:33 +0700

    finish sub bab Extensions

---
+++ src/main/kotlin/com/dicoding/exam/KotlinFunctional4/Extensions.kt
@@ -0,0 +1,33 @@
+package com.dicoding.exam.KotlinFunctional4
+
+fun main() {
+    10.printInt()
+    /*
+   output : value 10
+    */
+
+    println(10.plusThree())
+    /*
+   output : 13
+    */
+
+    println(10.slice)
+    /*
+   output : 5
+    */
+
+}
+
+fun Int.printInt() {
+    println("value $this")
+}
+
+fun Int.plusThree(): Int {
+    return this + 3
+}
+
+val Int.slice: Int
+    get() = this / 2
+
+
+
