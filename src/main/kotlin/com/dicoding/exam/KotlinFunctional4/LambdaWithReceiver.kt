e5ed7a496da9354ff6ec3e4d866e40d274373997
avrians <avriansyahb10@gmail.com>
2023-05-20 10:08:01 +0700

    finish sub bab Lambda with receiver

---
+++ src/main/kotlin/com/dicoding/exam/KotlinFunctional4/LambdaWithReceiver.kt
@@ -0,0 +1,21 @@
+package com.dicoding.exam.KotlinFunctional4
+
+fun main() {
+    val message = buildString {
+        append("Hello ")
+        append("from ")
+        append("lambda ")
+    }
+
+    println(message)
+}
+
+fun buildString(action: StringBuilder.() -> Unit): String {
+    val stringBuilder = StringBuilder()
+    stringBuilder.action()
+    return stringBuilder.toString()
+}
+
+/*
+   output : Hello from lambda
+*/
