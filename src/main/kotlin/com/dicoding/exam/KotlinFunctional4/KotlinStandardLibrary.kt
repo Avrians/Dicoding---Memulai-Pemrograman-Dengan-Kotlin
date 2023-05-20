313765569054e019a495eca3f51f3f7e578d6982
avrians <avriansyahb10@gmail.com>
2023-05-20 10:32:04 +0700

    finish sub bab Kotlin Standard Library

---
+++ src/main/kotlin/com/dicoding/exam/KotlinFunctional4/KotlinStandardLibrary.kt
@@ -0,0 +1,19 @@
+package com.dicoding.exam.KotlinFunctional4
+
+fun main() {
+    val buildString = StringBuilder().apply {
+        append("Hello ")
+        append("Kotlin ")
+    }
+
+    val text = "Hello"
+    text.let {
+        val message = "$it Kotlin"
+        println(message)
+    }
+
+    text.let { value ->
+        val message = "$value Kotlin"
+        println(message)
+    }
+}
