9a983926a393b1b2aaa4696abc96d110bb504d4c
avrians <avriansyahb10@gmail.com>
2023-05-23 16:13:21 +0700

    finish sub bab Object Everywhere

---
+++ src/main/kotlin/com/dicoding/exam/KotlinOOP5/ObjectEverywhere.kt
@@ -0,0 +1,30 @@
+package com.dicoding.exam.KotlinOOP5
+
+fun main() {
+    val someString = "Dicoding"
+    println(someString.reversed())
+    println(someString.toUpperCase())
+    println(someString.toLowerCase())
+    /*
+    Output:
+    gnidociD
+    DICODING
+    dicoding
+    */
+
+    val someString2 = "123"
+    val someInt = someString2.toInt()
+    val someOtherString = "12.34"
+    val someDouble = someOtherString.toDouble()
+
+    println(someInt is Int)
+    println(someDouble is Double)
+    /*
+    Output:
+
+    true
+    true
+    */
+
+}
+
