abd2c633bd16b5eab5f025c54fb866ab28da5b68
avrians <avriansyahb10@gmail.com>
2023-05-25 08:44:23 +0700

    finish sub bab Abstract Class

---
+++ src/main/kotlin/com/dicoding/exam/KotlinOOP5/AbstractClass.kt
@@ -0,0 +1,20 @@
+package com.dicoding.exam.KotlinOOP5
+
+abstract class Animal11(var name: String, var weight: Double, var age: Int, var isCarnivore: Boolean){
+
+    fun eat(){
+        println("$name sedang makan !")
+    }
+
+    fun sleep(){
+        println("$name sedang tidur !")
+    }
+}
+
+fun main(){
+    /*
+    Ketika kita mencoba membuat objek dari kelas Animal, akan terdapat eror berikut:
+    Cannot create an instance of an abstract class
+     */
+    //    val animal = Animal11("dicoding animal", 2.6, 1, true)
+}
